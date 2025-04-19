01c3e479db0b2621058bc78a4b66e0bbc3f2d649
Sepehr <sepehr.dh99@gmail.com>
2025-04-19 14:52:25 +0330

    feat: complete students controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,35 +1,49 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const {
+  getAllStudents,
+  addNewStudent,
+  getStudentDetail,
+  setStudentStatus,
+  updateStudent,
+} = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { userId, roleId, name } = req.query;
+  const students = await getAllStudents({ userId, roleId, name });
+  res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const payload = req.body;
+  const message = await addNewStudent(payload);
+  res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: userId } = req.params;
+  const payload = req.body;
+  const message = await updateStudent({ ...payload, userId });
+  res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id } = req.params;
+  const student = await getStudentDetail(id);
+  res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: userId } = req.params;
+  const { id: reviewerId } = req.user;
+  const { status } = req.body;
+  const message = await setStudentStatus({ userId, reviewerId, status });
+  res.json(message);
 });
 
 module.exports = {
-    handleGetAllStudents,
-    handleGetStudentDetail,
-    handleAddStudent,
-    handleStudentStatus,
-    handleUpdateStudent,
+  handleGetAllStudents,
+  handleGetStudentDetail,
+  handleAddStudent,
+  handleStudentStatus,
+  handleUpdateStudent,
 };
